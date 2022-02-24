3671828def3c6b4b282c5a69da0c64a4a87e1034
nicotralab <94816321+nicotralab@users.noreply.github.com>
2022-02-24 10:42:51 -0500

    Update and rename getPTvariants.sh to filterbyGATKBP.sh

--- filterbyGATKBP.sh
+++ filterbyGATKBP.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 
-
-
 # this will likely be specific to your system
 module load picard gatk/3.8.1 gcc/8.2.0 bcftools vcftools samtools
 
 # CREATE FASTA INDEX
 printf "*\n*\n*\nNow creating FASTA index with samtools faidx\n"
-samtools faidx startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa \
-	 -o startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa.fai
+samtools faidx genomeAssembly.fa \
+	 -o genomeAssembly.fa.fai
 
 
 # CREATE FASTA SEQUENCE DICTIONARY FILE
-printf "*\n*\n*\nNow creating FASTA sequence dictionary using GATK\n"
+printf "*\n*\n*\nNow creating FASTA sequence dictionary using Picard\n"
 java -Xmx64g \
-	 -jar /ihome/crc/install/picard/2.18.12/picard.jar CreateSequenceDictionary \
-	 R=startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa \
-	 O=startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.dict
+	 -jar picard.jar CreateSequenceDictionary \
+	 R=genomeAssembly.fa \
+	 O=genomeAssembly.dict
 
 # EXTRACT SNPS and INDELS INTO SEPARATE FILES
 #
-# The path to the .jar file will likely be specific to your system, and may not need to be specified.
 printf "*\n*\n*\nNow extracting SNPS\n"
 java -Xmx64g \
-     -jar /ihome/crc/install/gatk/GenomeAnalysisTK-3.8-1/GenomeAnalysisTK.jar \
+     -jar GenomeAnalysisTK.jar \
      -T SelectVariants \
-     -R startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa \
-	 -V pythonfiltered.vcf \
+     -R genomeAssembly.fa \
+     -V pythonfiltered.vcf \
      -selectType SNP \
      -o raw_snps.vcf
 
 printf "*\n*\n*\nNow extracting indels\n"
 java -Xmx64g \
-     -jar /ihome/crc/install/gatk/GenomeAnalysisTK-3.8-1/GenomeAnalysisTK.jar \
+     -jar GenomeAnalysisTK.jar \
      -T SelectVariants \
-     -R startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa \
+     -R genomeAssembly.fa \
      -V pythonfiltered.vcf \
      -selectType INDEL \
      -o raw_indels.vcf
@@ -42,12 +39,11 @@ java -Xmx64g \
 
 # HARD FILTERING SNPS and INDELS ACCORDING TO GATK BEST PRACTICES
 #
-# The path the to the .jar file will likely be specific to your system, and may not need to be specified.
 printf "*\n*\n*\nNow HARD filtering SNPs\n"
 java -Xmx64g \
-    -jar /ihome/crc/install/gatk/GenomeAnalysisTK-3.8-1/GenomeAnalysisTK.jar \
+    -jar GenomeAnalysisTK.jar \
     -T VariantFiltration \
-    -R startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa \
+    -R genomeAssembly.fa \
     -V raw_snps.vcf \
     --filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
     --filterName "GATKBP-snp" \
@@ -55,9 +51,9 @@ java -Xmx64g \
 
 printf "*\n*\n*\nNow HARD filtering indels\n"
 java -Xmx64g \
-    -jar /ihome/crc/install/gatk/GenomeAnalysisTK-3.8-1/GenomeAnalysisTK.jar \
+    -jar GenomeAnalysisTK.jar \
     -T VariantFiltration \
-    -R startingfiles/hsym.wildtype.canu.dovetail.pbjelly.arrow.pilon.primary.polished.renamed.fa \
+    -R genomeAssembly.fa \
     -V raw_indels.vcf \
     --filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
     --filterName "GATKBP-indel" \
@@ -72,8 +68,9 @@ bcftools view -f "PASS" indels.GATKBP-flagged.vcf > indels.GATKBP-passed.vcf
 rm indels.GATKBP-flagged.vcf
 
 
-
 # CONCATENATE THE TWO FILES, THEN SORT THEM
 vcf-concat snps.GATKBP-passed.vcf indels.GATKBP-passed.vcf > GATKBP-passed.unsorted.vcf
 vcf-sort GATKBP-passed.unsorted.vcf > GATKBP-passed.vcf
+
+# CLEAN UP
 rm GATKBP-passed.unsorted.vcf
